ad3cd12c0ae836b6a32688c08713023e325629b1
AMIR-KHT <codamir7814@gmail.com>
2025-01-29 17:10:19 +0330

    edit

--- src/Component/LandingComponent/HerosSction.jsx
+++ src/Component/LandingComponent/HerosSction.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import 'aos/dist/aos.css';
 import AOS from 'aos';
-import { useEffect } from 'react';
+import Typed from 'typed.js';
+import { useEffect, useRef } from 'react';
 
   
 
 const HerosSction = () => {
+  const text = useRef(null);
   useEffect(() => {
     AOS.init();
+      var typed = new Typed(text.current, { 
+        strings: ["Welcome to our journey of innovation and growth. Here, we showcase our passion, skills, and achievements, reflecting the hard work and dedication we've put into our projects. Explore our work and see how we transform ideas into reality, one project at a time"], 
+        typeSpeed: 20, 
+        backSpeed: 25, 
+        loop: false,
+        showCursor: false,
+      });
   }, []);
   return (
     <div id="hero-box" className="lg:w-10/12 md:w:10/12 w-full m-auto">
@@ -17,7 +26,7 @@ const HerosSction = () => {
           <div className="text-3xl">Hi &#x1F44B;</div>
           <div id="hi" className="mt-6 text-3xl typed">we are</div>
           <div className="mt-6"><span className="text-blue-700">SITCA</span> Develope Team</div>
-          <div  id="text" style={{fontFamily: "inter"}} class="lg:w-5/6 text-xl mt-6 text-justify w-full"></div>
+          <div  id="text" style={{fontFamily: "inter"}} class="lg:w-5/6 text-xl mt-6 text-justify w-full" ref={text}></div>
         </div>
         <div className="lg:w-2/3 w-full" data-aos="fade-left" data-aos-duration="900">
           <div id="logo-bg" style={{backgroundImage: 'url(./src/assets/Landing/images/logo-bg.png)', width: '560px', height: '520px'}}
