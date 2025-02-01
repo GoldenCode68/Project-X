b73f4fbda888476e5a0456aa4fae14d50e9efa7a
mahyar <mahi.mollaei86@gmail.com>
2025-02-01 21:10:45 +0330

    heder

--- src/Component/ProfileComponent/HeaderProf.jsx
+++ src/Component/ProfileComponent/HeaderProf.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeaderProf = () => {
   return (
-    <div id="header" className="w-10/12 m-auto mt-12 flex justify-between items-end">
+    <div className="w-10/12 m-auto mt-12 flex justify-between items-end">
       <div
         className="md:text-6xl md:to-50% text-4xl bg-gradient-to-r from-blue-700 from-10% to-purple-700 to-30% w-56 text-transparent bg-clip-text">
         SITCA</div>
