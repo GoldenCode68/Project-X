2ca50d2296a8b0130c59c1b63c29ab5be7dc634f
mahyar <mahi.mollaei86@gmail.com>
2025-02-10 23:02:24 +0330

    footer is finish

--- src/Component/ProfileComponent/MySocial.jsx
+++ src/Component/ProfileComponent/MySocial.jsx
@@ -15,28 +15,152 @@ const MySocial = () => {
         {/* top--1 */}
         <div className="w-full h-32 flex mb-4">
           {/* email */}
-          <div className="w-7/12 h-32 mr-2 rounded-3xl bg-blue-950">
-            <div className="border-2 w-44 h-20 flex">
-              <img className="border-2 w-30 h-20" src="./src/assets/Profile/img/.icon effects.png"/>
-              <img className="border-2 w-10 h-16"/>
+          <div className="w-7/12 h-32 mr-2 rounded-3xl bg-blue-950 flex">
+            {/* part one */}
+            <div className="w-36 h-20 flex">
+              <img
+                className="w-22 h-14 ml-7 mt-3"
+                src="./src/assets/Profile/img/.icon effects.png"
+              />
+              <img
+                className="w-6 h-6 ml-3 mt-3"
+                src="./src/assets/Profile/img/External_00Link.png"
+              />
+            </div>
+            {/* part tow */}
+            <div className="w-72 h-20 ml-3">
+              <div className="w-20 h-5 mt-4" style={{ fontFamily: "space" }}>
+                Email
+              </div>
+              <div className="w-60 h-5 mt-1" style={{ fontFamily: "inter" }}>
+                satarsilent@gmail.com
+              </div>
             </div>
           </div>
           {/* figma */}
-          <div className="w-5/12 h-32 ml-2 rounded-3xl bg-gradient-to-b from-zinc-800 to-slate-950"></div>
+          <div className="w-5/12 h-32 ml-2 rounded-3xl bg-gradient-to-b from-zinc-800 to-slate-950 flex">
+            {/* part one */}
+            <div className="w-32 h-20 flex">
+              <img
+                className="w-22 h-14 ml-7 mt-3"
+                src="./src/assets/Profile/img/Main.png"
+              />
+              <img
+                className="w-6 h-6 ml-3 mt-3"
+                src="./src/assets/Profile/img/External_00Link.png"
+              />
+            </div>
+            {/* part tow */}
+            <div className="w-72 h-20">
+              <div className="w-20 h-5 mt-4" style={{ fontFamily: "space" }}>
+                Figma
+              </div>
+              <div className="w-60 h-5 mt-1" style={{ fontFamily: "inter" }}>
+                @aqsam
+              </div>
+            </div>
+          </div>
         </div>
         {/* top--2 */}
         {/* instagram */}
-        <div className="w-full h-32 mb-4 rounded-3xl bg-gradient-to-br via-blue-500 from-sky-500 via-yellow-500 via-red-500 to-purple-500 "></div>
+        <div className="flex w-full h-32 mb-4 rounded-3xl bg-gradient-to-br via-blue-500 from-sky-500 via-yellow-500 via-red-500 to-purple-500 ">
+          {/* part one */}
+          <div className="w-36 h-20 flex">
+            <img
+              className="w-22 h-14 ml-7 mt-3"
+              src="./src/assets/Profile/img/.icon effect00s.png"
+            />
+            <img
+              className="w-6 h-6 ml-3 mt-3"
+              src="./src/assets/Profile/img/External_00Link.png"
+            />
+          </div>
+          {/* part tow */}
+          <div className="w-72 h-20 ml-3">
+            <div className="w-32 h-5 mt-4" style={{ fontFamily: "space" }}>
+              Instagram
+            </div>
+            <div className="w-60 h-5 mt-1" style={{ fontFamily: "inter" }}>
+              @dev.aqsam
+            </div>
+          </div>
+        </div>
         {/* top--3 */}
-        <div className=" w-full h-32 flex mb-4">
+        <div className="w-full h-32 flex mb-4">
           {/* linkdin */}
-          <div className="w-7/12 h-32 mr-2 rounded-3xl bg-gradient-to-b from-cyan-300 to-sky-600"></div>
+          <div className="w-7/12 h-32 mr-2 rounded-3xl bg-gradient-to-b from-cyan-300 to-sky-600 flex">
+            {/* part one */}
+            <div className="w-36 h-20 flex">
+              <img
+                className="w-22 h-14 ml-7 mt-3"
+                src="./src/assets/Profile/img/Mai00n.png"
+              />
+              <img
+                className="w-6 h-6 ml-3 mt-3"
+                src="./src/assets/Profile/img/External_00Link.png"
+              />
+            </div>
+            {/* part tow */}
+            <div className="w-72 h-20 ml-3">
+              <div className="w-20 h-5 mt-4" style={{ fontFamily: "space" }}>
+                Linkedin
+              </div>
+              <div className="w-60 h-5 mt-1" style={{ fontFamily: "inter" }}>
+                @m_aqsam11
+              </div>
+            </div>
+          </div>
           {/* dev */}
-          <div className="w-5/12 h-32 ml-2 rounded-3xl bg-gradient-to-b from-gray-50 to-gray-200"></div>
+          <div className="w-5/12 h-32 ml-2 rounded-3xl bg-gradient-to-b from-gray-50 to-gray-200 flex">
+            {/* part one */}
+            <div className="w-36 h-20 flex">
+              <img
+                className="w-22 h-14 ml-7 mt-3"
+                src="./src/assets/Profile/img/.icon effec00ts.png"
+              />
+              <img
+                className="w-5 h-5 ml-3 mt-3"
+                src="./src/assets/Profile/img/Vect00or.png"
+              />
+            </div>
+            {/* part tow */}
+            <div className="w-72 h-20 ml-3 text-black">
+              <div className="w-40 h-5 mt-4 " style={{ fontFamily: "space" }}>
+                Dev-Community
+              </div>
+              <div
+                className="w-60 h-5 mt-1 text-black"
+                style={{ fontFamily: "inter" }}
+              >
+                @maqsam
+              </div>
+            </div>
+          </div>
         </div>
         {/* top--4 */}
         {/* github */}
-        <div className="w-full h-32 rounded-3xl bg-gradient-to-t from-black to-gray-800"></div>
+        <div className="w-full h-32 rounded-3xl bg-gradient-to-t from-black to-gray-800 flex">
+          {/* part one */}
+          <div className="w-36 h-20 flex">
+            <img
+              className="w-22 h-14 ml-7 mt-3"
+              src="./src/assets/Profile/img/Frame 229.png"
+            />
+            <img
+              className="w-6 h-6 ml-3 mt-3"
+              src="./src/assets/Profile/img/External_00Link.png"
+            />
+          </div>
+          {/* part tow */}
+          <div className="w-72 h-20 ml-3">
+            <div className="w-20 h-5 mt-4" style={{ fontFamily: "space" }}>
+              GitHub
+            </div>
+            <div className="w-60 h-5 mt-1" style={{ fontFamily: "inter" }}>
+              senator/github.com
+            </div>
+          </div>
+        </div>
       </div>
       {/*  */}
     </div>
