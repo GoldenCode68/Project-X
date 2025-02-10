492923c8bfa69648359fe679866e8fe854d4c330
mahyar <mahi.mollaei86@gmail.com>
2025-02-10 23:15:30 +0330

    window scrollTo finish

--- src/Screen/Profile/Profile.jsx
+++ src/Screen/Profile/Profile.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import HeaderProf from '../../Component/ProfileComponent/HeaderProf'
-import MyProf from '../../Component/ProfileComponent/MyProf'
-import MySkills from '../../Component/ProfileComponent/MySkills'
-import MyPortfolio from '../../Component/ProfileComponent/MyPortfolio'
-import MySocial from '../../Component/ProfileComponent/MySocial'
-import FooterProf from '../../Component/ProfileComponent/FooterProf'
+import React, { useEffect } from "react";
+import HeaderProf from "../../Component/ProfileComponent/HeaderProf";
+import MyProf from "../../Component/ProfileComponent/MyProf";
+import MySkills from "../../Component/ProfileComponent/MySkills";
+import MyPortfolio from "../../Component/ProfileComponent/MyPortfolio";
+import MySocial from "../../Component/ProfileComponent/MySocial";
+import FooterProf from "../../Component/ProfileComponent/FooterProf";
 
 const Profile = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
-      <HeaderProf/>
-      <MyProf/>
-      <MySkills/>
-      <MyPortfolio/>
-      <MySocial/>
-      <FooterProf/>
+      <HeaderProf />
+      <MyProf />
+      <MySkills />
+      <MyPortfolio />
+      <MySocial />
+      <FooterProf />
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
