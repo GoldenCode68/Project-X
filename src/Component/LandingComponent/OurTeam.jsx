9015b34f9b11622e851d3676357d9757b25e6b24
AMIR-KHT <codamir7814@gmail.com>
2025-02-22 15:17:08 +0330

    new Responsive

--- src/Component/LandingComponent/OurTeam.jsx
+++ src/Component/LandingComponent/OurTeam.jsx
@@ -22,24 +22,24 @@ const OurTeam = () => {
         </div>
         <div
           id="team"
-          className="3xl:w-10/12 w-full m-auto flex justify-around flex-wrap mt-8"
+          className="3xl:w-10/12 w-full m-auto flex lg:justify-around md:justify-around justify-evenly flex-wrap mt-8"
         >
           {data.people.map((person, index) => (
             <div data-aos="fade-up" data-aos-duration="900">
               <Link to={"/Profile"}>
                 <div
                   key={index}
-                  className="m-auto students box-border relative flex items-center flex-col justify-around mt-10"
+                  className="m-auto lg:w-[366px] lg:h-[245px] md:w-[366px] md:h-[245px] w-[170px] h-[122px] students box-border relative flex items-center flex-col justify-around mt-10"
                 >
-                  <div className="imgs mt-3">
+                  <div className="imgs mt-3 lg:w-[149px] lg:h-[149px] md:w-[149px] md:h-[149px] w-[52px] h-[52px]">
                     <img src={img} alt="" />
                   </div>
-                  <div className="names text-xl tracking-wider">
+                  <div className="names lg:text-xl md:text-xl text-[12px] tracking-wider">
                     {person.name}
                   </div>
                   <div
                     id="personSkill"
-                    className="skills relative -top-3 text-lg tracking-widest"
+                    className="skills relative lg:-top-3 md:-top-3 -top-2 lg:text-lg md:text-lg text-[9px] tracking-widest"
                   >
                     (Junior{" "}
                     <span className="bg-gradient-to-br from-blue-700 from-10% to-purple-700 to-100%0% text-transparent bg-clip-text">
