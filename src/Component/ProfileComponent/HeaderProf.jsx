41aa8aff119d1074157958e2b4a16d3bf734b9cb
mahyar <mahi.mollaei86@gmail.com>
2025-02-23 12:44:02 +0330

    finish logo header

--- src/Component/ProfileComponent/HeaderProf.jsx
+++ src/Component/ProfileComponent/HeaderProf.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/Profile/img/logo.png";
+import logo from "../../assets/logo.png";
 
 const HeaderProf = () => {
   return (
