dda4b321d5b4dd65b2245ee2614373326d303677
mahyar <mahi.mollaei86@gmail.com>
2025-02-25 21:14:22 +0330

    finish responsiv of header

--- src/Component/LandingComponent/Header.jsx
+++ src/Component/LandingComponent/Header.jsx
@@ -3,13 +3,12 @@ import logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
-    <div
-      id="header"
-      className="w-10/12 m-auto mt-12 flex justify-between items-end"
-    >
-      <div className="w-24">
-        <img src={logo} />
-      </div>
+    <div className="w-10/12 m-auto mt-12 flex items-end">
+        <div className="w-20 imgheader">
+          <img src={logo} />
+        </div>
+        <span className="textheader text-7xl ml-2 bg-gradient-to-tl from-blue-700 from-10% to-purple-700 to-100%0% text-transparent bg-clip-text" style={{ fontFamily: "poppins" }}>REE</span>
+        <span className="textheader text-7xl bg-gradient-to-br from-blue-700 from-10% to-purple-700 to-100%0% text-transparent bg-clip-text" style={{ fontFamily: "satoshi" }}>VAN</span>
     </div>
   );
 };
--- src/Component/ProfileComponent/HeaderProf.jsx
+++ src/Component/ProfileComponent/HeaderProf.jsx
@@ -4,11 +4,13 @@ import logo from "../../assets/logo.png";
 
 const HeaderProf = () => {
   return (
-    <div className="w-10/12 m-auto mt-12 flex justify-between items-end">
-      <Link to={"/"}>
-        <div className="w-24">
+    <div className="w-10/12 m-auto mt-12 flex items-end">
+      <Link to={"/"} className="flex">
+        <div className="w-20 imgheader">
           <img src={logo} />
         </div>
+        <span className="textheader mt-2 text-7xl ml-2 bg-gradient-to-tl from-blue-700 from-10% to-purple-700 to-100%0% text-transparent bg-clip-text" style={{ fontFamily: "poppins" }}>REE</span>
+        <span className="textheader mt-2 text-7xl bg-gradient-to-br from-blue-700 from-10% to-purple-700 to-100%0% text-transparent bg-clip-text" style={{ fontFamily: "satoshi" }}>VAN</span>
       </Link>
     </div>
   );
